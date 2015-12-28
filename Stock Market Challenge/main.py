e57437ae9323bb6b3d27511b5eb3910944e5a84b
liangz0707 <liangzh0707@gmail.com>
2015-12-28 18:44:34 +0800

    做了股票预测，效率很差

--- Stock Market Challenge/main.py
+++ Stock Market Challenge/main.py
@@ -3,21 +3,12 @@ __author__ = 'liangz14'
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+from sklearn import linear_model
 import matplotlib.pyplot as plt
 from xgboost.sklearn import XGBRegressor
 import numpy as np
 
-df_train = pd.read_csv("train.csv")
-df_test = pd.read_csv("test.csv")
 
-df_train = df_train.fillna(0)
-df_train_feature = df_train[df_train.columns[1:26]]
-
-#print df_train_feature
-#print df_train_feature.columns
-#print df_train_feature.size
-
-c = df_train_feature.values
 
 def KMeans_classify():
     random_state = 170
@@ -46,8 +37,111 @@ def KMeans_classify():
     plt.scatter(list(c_2D[0][mask]),list(c_2D[1][mask]),c='b')
 
     plt.show()
-'''
-    这里声明了XBG树，但是突然想到，这是无监督分类所以不能用！
-'''
+
+
 def xbgclassify():
+    '''
+    这里声明了XBG树，但是突然想到，这是无监督分类所以不能用！
+    '''
     xgb = XGBRegressor(max_depth=6, learning_rate=0.3, n_estimators=25, subsample=0.5, colsample_bytree=0.5, seed=0)
+
+
+def regression_lasso(train_list,target_list,test_list,mean):
+    """
+    直接使用数据进行预测
+    :param train_list:
+    :param test_list:
+    :return:
+    """
+    clf = linear_model.LassoLars(alpha=.01)
+    clf.fit(train_list, target_list)
+    result_list = clf.predict(test_list)+mean
+    return result_list
+
+
+def data_filter(train_list, target_list, test_list):
+    train_list = np.asarray(train_list, dtype=float)
+    target_list = np.asarray(target_list, dtype=float)
+    test_list = np.asarray(test_list, dtype=float)
+
+    target_mean = np.mean(target_list, axis=0)
+    train_mean = np.mean(train_list, axis=0)
+
+    target_list = target_list - target_mean
+    train_list = train_list - train_mean
+
+    train_var = np.var(train_list, axis=0)
+
+    train_list = train_list/train_var
+
+    test_list = (test_list - train_mean)/train_var
+
+    print train_list.shape
+    print target_list.shape
+    print test_list.shape
+
+    return train_list, target_list, test_list, target_mean
+
+def run_result():
+    df_train = pd.read_csv("train.csv")
+    df_test = pd.read_csv("test_2.csv")
+
+    df_train = df_train.fillna(0)
+    df_test = df_test.fillna(0)
+    print len(df_train.columns)
+    print len(df_test.columns)
+    df_train_feature = df_train[df_train.columns[26:]]
+    df_test_feature = df_test[df_test.columns[26:]]
+
+    print len(df_train_feature.columns)
+    print len(df_test_feature.columns)
+    train_list = df_train[df_train_feature.columns[:121]].values.astype(np.float32)
+    target_list = df_train[df_train_feature.columns[121:-2]].values.astype(np.float32)
+    test_list = df_test[df_test_feature.columns[:]].values.astype(np.float32)
+
+    train_list, target_list, test_list,mean = data_filter(train_list, target_list, test_list)
+
+    result_list = regression_lasso(train_list, target_list, test_list, mean)
+
+    result_list = result_list.reshape((-1,))
+    print result_list.shape
+    f = open('result.csv','w')
+    f.write("id,Predicted\n")
+    for i,v in enumerate(result_list):
+        f.write("%d_%d,%f\n" % (i/62+1,i%62+1,v))
+
+    f.close()
+
+def show_state():
+    df_train = pd.read_csv("train.csv")
+    #df_test = pd.read_csv("test_2.csv")
+
+    df_train = df_train.fillna(0)
+    #df_test = df_test.fillna(0)
+    #print len(df_train.columns)
+    # print len(df_test.columns)
+
+    df_train_feature = df_train[df_train.columns[28:-4]]
+    print df_train_feature.columns
+    data = df_train_feature.values
+
+    data_mean = np.mean(data, axis=0)
+
+    data_list = data - data_mean
+
+    data_var = np.var(data_list, axis=0)
+
+    data = data_list/data_var
+
+    for i,d in enumerate(data):
+        for j in range(1,len(d)):
+            d[j] = d[j] + d[j-1]
+        data[i] = d
+    c = data[1:30000,:].T
+    print c.shape
+
+    plt.plot(c)
+    plt.show()
+
+
+show_state()
